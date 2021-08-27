f60cbb3d9eeba8872171b86f55891d5ecef3843b
Nick Stockton <phoenix76728@gmail.com>
2021-08-27 14:56:34 -0400

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        public class Car
+        {
+            public Car(string make, string model, int year)
+            {
+                Console.WriteLine($"the make is {make} the model is {model} and the year is {year} of this car.");
+            }
+               
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is the make of the car");
+            string make = Console.ReadLine();
+            Console.WriteLine("what is the make of the car");
+            string model = Console.ReadLine();
+            Console.WriteLine("what is the make of the car");
+            int year = Convert.ToInt32(Console.ReadLine());
+            object car1 = new Car(make, model, year);
+        }
+    }
+}
